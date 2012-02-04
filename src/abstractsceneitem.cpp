fe25074975ac5d0735380b88075dd7c6b60083a5
m.navrotskiy <m.navrotskiy@localhost>
2012-02-04 20:08:52 +0400

    ?????????? ???????????? ?????? ??????? ????? ?????? ??????.

--- src/abstractsceneitem.cpp
+++ src/abstractsceneitem.cpp
@@ -1,23 +1,5 @@
 #include "abstractsceneitem.h"
 
-/* Конструктор с параметрами. */
-AbstractSceneItem::AbstractSceneItem(const int x, const int y)
-    : QGraphicsItem()
+AbstractSceneItem::AbstractSceneItem()
 {
-#ifdef QT_DEBUG
-    logMessage("AbstractSceneItem::AbstractSceneItem()\n");
-#endif
-
-    m_xCoord = x;
-    m_yCoord = y;
-}
-
-/* Метод расчета и получения области, занимаемой элементом сцены. */
-QRectF AbstractSceneItem::boundingRect() const
-{
-#ifdef QT_DEBUG
-    logMessage("AbstractSceneItem::boundingRect()\n");
-#endif
-
-    return QRectF(QPointF(m_xCoord,m_yCoord),QSizeF(30,30));
 }
