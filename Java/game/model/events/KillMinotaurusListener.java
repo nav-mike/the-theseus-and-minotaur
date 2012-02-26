4b5730ece651c1f11fe7b1bd7332521cc9a81abf
Mikhail Navrotskiy <Mikhail Navrotskiy@MikhailNavrotsk>
2012-02-26 21:49:07 +0400

    ???????? ????? ??????????? ???????, ???????????? ????? ????? ??????? ?????????.

---
+++ Java/game/model/events/KillMinotaurusListener.java
@@ -0,0 +1,17 @@
+package game.model.events;
+
+import java.util.EventListener;
+
+/**
+ * Интерфейс обработчика события убийства Минотавра Тесеем.
+ */
+public interface KillMinotaurusListener extends EventListener {
+    
+    /* Методы интерфейса. */
+    /**
+     * Метод обработки события убийства Минотавра Тесеем.
+     * @param e Событие.
+     */
+    void minotaurusDead (KillMinotaurusEvent e);
+    
+}
