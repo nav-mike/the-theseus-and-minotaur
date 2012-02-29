2c9da4f52e6344a9ef41d51b7e1a1e03fa5e5ac1
Mikhail Navrotskiy <Mikhail Navrotskiy@MikhailNavrotsk>
2012-02-29 09:52:36 +0400

    ? ????? ?????? ????????? ??????? ?????????? ???????? ??????????? ?? ?????????.

--- Java/game/graphics/MyMenuBar.java
+++ Java/game/graphics/MyMenuBar.java
@@ -18,6 +18,22 @@ public class MyMenuBar {
     private ArrayList<MyMenuBarItem> m_items;
 
     /**
+     * Конструктор с параметрами.
+     * Создает список из трех элементов:
+     *  1. Наличие меча у Тесея.
+     *  2. Наличие ключа у Тесея.
+     *  3. Количество очков хода у Тесея.
+     */
+    public MyMenuBar () {
+
+        m_items = new ArrayList<MyMenuBarItem>();
+
+        m_items.add(new MyMenuBarItem("Наличие у Тесея меча:","нет",10,10));
+        m_items.add(new MyMenuBarItem("Наличие у Тесея ключа:","нет",20,20));
+        m_items.add(new MyMenuBarItem("Количество очков хода у Тесея:", "2",30,30));
+    }
+
+    /**
      * Метод добавления элемента в список.
      * @param item Элемент игровой информации.
      */
